0ad5e0e7b2c6ecbaa746f38ffb693c34adfe4e62
Lam Nguyen <lamitptit99@gmail.com>
2020-05-09 10:18:54 +0700

    commit- 9/5 completation camera

--- app/src/main/java/com/example/cameraxp/MainActivity.kt
+++ app/src/main/java/com/example/cameraxp/MainActivity.kt
@@ -12,18 +12,20 @@ import android.view.Surface
 import android.view.TextureView
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import java.io.File
+import java.nio.ByteBuffer
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), LifecycleOwner {
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var viewFinder: TextureView
-
     private val REQUEST_CODE_PERMISSIONS = 10
     private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,11 +33,12 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         setContentView(R.layout.activity_main)
 
         viewFinder = findViewById(R.id.view_finder)
-
         // Request camera permissions
         if (allPermissionsGranted()) {
             viewFinder.post { startCamera() }
+
         } else {
+            //nếu các permission trong manifest chưa dược granted thì cần phải yêu cầu lại
             ActivityCompat.requestPermissions(
                     this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
@@ -48,7 +51,7 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
 
     private fun startCamera() {
         val previewConfig = PreviewConfig.Builder().apply {
-            setTargetResolution(Size(640, 480))
+            setTargetResolution(Size(1280, 720 ))
         }.build()
 
 
@@ -60,18 +63,19 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
 
             // To update the SurfaceTexture, we have to remove it and re-add it
             val parent = viewFinder.parent as ViewGroup
+
             parent.removeView(viewFinder)
             parent.addView(viewFinder, 0)
-
             viewFinder.surfaceTexture = it.surfaceTexture
             updateTransform()
         }
+        //set độ phân giải cho ảnh
         val imageCaptureConfig = ImageCaptureConfig.Builder()
             .apply {
                 // We don't set a resolution for image capture; instead, we
                 // select a capture mode which will infer the appropriate
                 // resolution based on aspect ration and requested mode
-                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
             }.build()
 
         // Build the image capture use case and attach button click listener
@@ -103,7 +107,21 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
                     }
                 })
         }
-        CameraX.bindToLifecycle(this, preview,imageCapture)
+
+        //phân tích in ra độ sáng trung bình của bức ảnh
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(
+                //phân tích ảnh gần nhất
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
+
+        // Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, LuminosityAnalyzer())
+        }
+        CameraX.bindToLifecycle(this, preview,imageCapture,analyzerUseCase)
     }
 
     private fun updateTransform() {
@@ -126,11 +144,7 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         // Finally, apply transformations to our TextureView
         viewFinder.setTransform(matrix)
     }
-
-    /**
-     * Process result from permission request dialog box, has the request
-     * been granted? If yes, start Camera. Otherwise display a toast
-     */
+    //kiểm tra
     override fun onRequestPermissionsResult(
             requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
@@ -145,11 +159,46 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
         }
     }
 
-    /**
-     * Check if all permission specified in the manifest have been granted
-     */
+    //kiểm tra tât cả các permission
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
                 baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
+
+//phân tích
+private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+    private var lastAnalyzedTimestamp = 0L
+
+    /**
+     * Helper extension function used to extract a byte array from an
+     * image plane buffer
+     */
+    private fun ByteBuffer.toByteArray(): ByteArray {
+        rewind()    // Rewind the buffer to zero
+        val data = ByteArray(remaining())
+        get(data)   // Copy the buffer into a byte array
+        return data // Return the byte array
+    }
+
+    override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+        val currentTimestamp = System.currentTimeMillis()
+        // Calculate the average luma no more often than every second
+        if (currentTimestamp - lastAnalyzedTimestamp >=
+            TimeUnit.SECONDS.toMillis(1)) {
+            // Since format in ImageAnalysis is YUV, image.planes[0]
+            // contains the Y (luminance) plane
+            val buffer = image.planes[0].buffer
+            // Extract image data from callback object
+            val data = buffer.toByteArray()
+            // Convert the data into an array of pixel values
+            val pixels = data.map { it.toInt() and 0xFF }
+            // Compute average luminance for the image
+            val luma = pixels.average()
+            // Log the new luma value
+            Log.d("CameraXApp", "Average luminosity: $luma")
+            // Update timestamp of last analyzed frame
+            lastAnalyzedTimestamp = currentTimestamp
+        }
     }
 }
---
+++ app/src/main/java/com/example/cameraxp/TestCameraX.kt
@@ -0,0 +1,12 @@
+package com.example.cameraxp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestCameraX : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_camera_x)
+    }
+}
